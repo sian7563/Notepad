46168f29ea5a364b68bc240486021a16945742bb
sian7563 <sian7563@naver.com>
2021-06-17 16:55:03 +0900

    메모장26영상 시청 후 제작

--- NotePad1/NotePad1/NotePad1/DetailViewController.swift
+++ NotePad1/NotePad1/NotePad1/DetailViewController.swift
@@ -22,11 +22,16 @@ class DetailViewController: UIViewController {
     }()
     
     
-    @IBAction func share(_ sender: Any) {
+    @IBAction func share(_ sender: UIBarButtonItem) {
         
         guard let memo = memo?.content else { return }
         
         let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
+        
+        if let pc = vc.popoverPresentationController {
+            pc.barButtonItem = sender
+        }
+        
         present(vc, animated: true, completion: nil)
     }
     
